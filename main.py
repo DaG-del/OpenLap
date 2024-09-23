6bb9d588a16f6fbd765fa42687cda0544838a4ba
gautam <gautamgautm123@gmail.com>
2024-09-23 20:12:23 +0530

    update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+f = open("brake.csv", "r")
+f = f.readlines()
+line = []
+
+for ef in range(len(f)):
+    line.append(f[ef].split(",")[2])
+
+br_disc_d = float(line[0])/1000
+br_pad_h = float(line[1])/1000
+br_pad_mu = float(line[2])
+br_nop = float(line[3])
+br_pist_d = float(line[4])
+br_mast_d = float(line[5])
+br_ped_r = float(line[6])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+br_pist_a = math.pow(br_nop*math.pi*(br_pist_d/1000), 2)/4
+br_mast_a = math.pow(math.pi*(br_mast_d/1000), 2)/4
+beta = tyre_radius/(br_disc_d/2 - br_pad_h/2)/br_pist_a/br_pad_mu/4
+phi = br_mast_a/br_ped_r*2
