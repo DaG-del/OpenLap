552dcc057aac970ce070d73ad359b03445c55330
gautam <gautamgautm123@gmail.com>
2024-09-24 18:12:13 +0530

    update

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ line = []
 for ef in range(len(f)):
     line.append(f[ef].split(",")[2])
 
+factor_grip = float(line[0])
+tyre_radius = float(line[1])
+Cr = float(line[2])
+mu_x = float(line[3])
+mu_x_M = float(line[4])
+sens_x = float(line[5])
+mu_y = float(line[6])
+mu_y_M = float(line[7])
+sens_y = float(line[8])
+CF = float(line[9])
+CR = float(line[10])
+
+f = open("brake.csv", "r")
+f = f.readlines()
+line = []
+
+for ef in range(len(f)):
+    line.append(f[ef].split(",")[2])
+
 br_disc_d = float(line[0])/1000
 br_pad_h = float(line[1])/1000
 br_pad_mu = float(line[2])
@@ -14,7 +33,6 @@ br_nop = float(line[3])
 br_pist_d = float(line[4])
 br_mast_d = float(line[5])
 br_ped_r = float(line[6])
-
 
 br_pist_a = math.pow(br_nop*math.pi*(br_pist_d/1000), 2)/4
 br_mast_a = math.pow(math.pi*(br_mast_d/1000), 2)/4
