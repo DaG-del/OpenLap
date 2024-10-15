4a07cad619b3fbe82d1d3f141f6b9d15beb3663a
gautam <gautamgautm123@gmail.com>
2024-10-15 13:27:08 +0530

    update

--- helper_lib.py
+++ helper_lib.py
@@ -16,3 +16,92 @@ def vector(min, dv, v_max):
         curr += dv
 
     return n
+
+def compare_floats(a, b, error):
+    a_rounded = []
+    for ay in a:
+        a_rounded.append(round(ay, 2))
+    b_rounded = []
+    for by in b:
+        b_rounded.append(round(by, 2))
+
+    if len(b_rounded) != len(a_rounded):
+        print("Unequal length!")
+        return
+
+    errors = {}
+    for x in range(len(a_rounded)):
+        if a_rounded[x] - b_rounded[x] > error:
+            errors[x] = [a_rounded[x], b_rounded[x]]
+
+    for e in errors:
+        print(e, end = " : ")
+        print(errors[e])
+
+
+def read_csv(filename):
+    f = open(filename, "r")
+    ret = []
+    ip = f.readlines()
+    for i in ip:
+        ret.append(float(i))
+
+    return ret
+
+
+def rotz(angle, dsv, p):
+    angle *= math.pi/180
+    x = dsv*math.cos(angle) + p[0]
+    y = dsv*math.sin(angle) + p[1]
+
+    return [x, y]
+
+
+def absol(r: list):
+    r_abs = []
+    for aar in r:
+        r_abs.append(abs(aar))
+    return r_abs
+
+
+def find(r_apex_indices, r):
+    ret = []
+    for aar in r_apex_indices:
+        ret.append(r[aar])
+    return ret
+
+
+def calc_d(x, total_length, Y):
+    '''
+    % linear correction vectors
+    DX = x/L*(X(1)-X(end)) ;
+    DY = x/L*(Y(1)-Y(end)) ;
+    DZ = x/L*(Z(1)-Z(end)) ;
+    % adding correction
+    X = X+DX ;
+    Y = Y+DY ;
+    Z = Z+DZ ;
+    '''
+    ret = []
+    DX = []
+    for el in range(len(x)):
+        DX.append(x[el] / total_length * (Y[0] - Y[-1]))
+
+    for el in range(len(DX)):
+        ret.append(Y[el] + DX[el])
+
+    return ret
+
+
+def writecsv(r_apex, distance_step_vector, n, r, x, X, Y, Z):
+    f = open("OpenTRACK_output.csv", "a")
+    d = {}
+    d["r_apex"] = r_apex
+    d["dx"] = distance_step_vector
+    d["n"] = n
+    d["r"] = r
+    d["x"] = x
+    d["X"] = X
+    d["Y"] = Y
+    d["Z"] = Z
+    f.write(str(d))
--- main.py
+++ main.py
@@ -1,8 +1,249 @@
 import math
+
+import scipy
+
 import helper_lib
 import matplotlib.pyplot as mlt
 import numpy
 from scipy.interpolate import interp1d
+
+
+def center_points(l):
+    sum = 0
+    cumulative = []
+    for el in l:
+        sum += el
+        cumulative.append(sum - el / 2)
+
+    return cumulative
+
+
+def end_points(l):
+    sum = 0
+    cumulative = []
+    for el in l:
+        sum += el
+        cumulative.append(sum)
+
+    return cumulative
+
+
+def nos(R):
+    n = 0
+    for r in R:
+        if r == float("inf"):
+            n += 1
+
+    return n
+
+
+def fine(total_length):
+    tl = total_length
+    x_fine = []
+    sum = 0
+    while True:
+        x_fine.append(sum)
+        sum += _mesh_size
+        if x_fine[-1] > math.floor(tl):
+            x_fine.pop()
+            x_fine.append(total_length)
+            return x_fine
+
+
+def min_custom(dh):
+    dh_abs = []
+    for d in dh:
+        dh_abs.append(abs(d))
+    return min(dh_abs)
+
+
+_KAPPA = 10
+_mesh_size = 0.1
+f = open("track.txt", "r")
+track = []
+ip = f.readlines()
+
+for l in ip:
+    l = l[:-1]
+    tup = tuple(l.split(","))
+    track.append(tup)
+
+track = tuple(track)
+
+R = [float(t[2]) for t in track]
+for r in range(len(R)):
+    if R[r] == 0:
+        R[r] = float("inf")
+
+l = [float(t[1]) for t in track]
+type = [-int(t[0]) for t in track]
+total_length = sum(l)
+
+track = [(type[i], l[i], R[i]) for i in range(len(track))]
+
+angle_seg = [(l[i] / R[i]) * 180 / math.pi for i in range(len(track))]
+
+R_injected = R
+l_injected = l
+type_injected = type
+
+j = 0
+for i in range(len(l)):
+    if angle_seg[i] > _KAPPA:
+        distance_of_injected_point_from_corner_entry_and_exit = min(l_injected[j] / 3, (_KAPPA * math.pi / 180) * R[i])
+        temp = l_injected[0:j]
+        temp.append(distance_of_injected_point_from_corner_entry_and_exit)
+        temp.append(l_injected[j] - 2 * distance_of_injected_point_from_corner_entry_and_exit)
+        temp.append(distance_of_injected_point_from_corner_entry_and_exit)
+        temp.extend(l_injected[j + 1:])
+        l_injected = temp
+
+        temp = R_injected[0:j]
+        temp.extend([R_injected[j]] * 3)
+        temp.extend(R_injected[j + 1:])
+
+        R_injected = temp
+
+        temp = type_injected[0:j]
+        temp.extend([type_injected[j]] * 3)
+        temp.extend(type_injected[j + 1:])
+
+        type_injected = temp
+
+        del temp
+
+        j += 3
+    else:
+        j += 1
+
+R = R_injected
+l = l_injected
+type = type_injected
+
+'''
+for i=1:length(l)-1
+    j = 1 ;
+    while true
+        if type(i+j)==0 && type(i)==0 && l(i)~=-1
+            l(i) = l(i)+l(i+j) ;
+            l(i+j) = -1 ;
+        else
+            break
+        end
+        j = j+1 ;
+    end
+end
+R(l==-1) = [] ;
+type(l==-1) = [] ;
+l(l==-1) = [] ;
+'''
+
+for i in range(len(l) - 1):
+    j = 1
+    while True:
+        if type[i + j] == 0 and type[i] == 0 and l[i] != -1:
+            l[i] = l[i + j] + l[i]
+            l[i + j] = -1
+        else:
+            break
+        j += 1
+
+temp = len(l) - 1
+el = 0
+while el < temp:
+    if l[el] == -1:
+        l.pop(el)
+        R.pop(el)
+        type.pop(el)
+        temp -= 1
+    el += 1
+
+segment_end_point = end_points(l)
+
+segment_center_point = center_points(l)
+
+no_of_straights = nos(R)
+x_coarse = [0] * (len(segment_end_point) + no_of_straights)
+r = [0] * len(x_coarse)
+
+j = 0
+for i in range(len(segment_center_point)):
+    if R[i] == float("inf"):
+        x_coarse[j] = segment_end_point[i] - l[i]
+        x_coarse[j + 1] = segment_end_point[i]
+        j += 2
+    else:
+        x_coarse[j] = segment_center_point[i]
+        r[j] = type[i] / R[i]
+        j += 1
+
+x = fine(total_length)
+
+distance_step_vector = numpy.diff(x)
+distance_step_vector = distance_step_vector.tolist()
+distance_step_vector.append(distance_step_vector[-1])
+number_of_mesh_points = len(x)
+
+r = scipy.interpolate.pchip_interpolate(x_coarse, r, x)
+r = r.tolist()
+
+n = len(x)
+
+X = [0] * n
+Y = [0] * n
+
+angle_seg = []
+for d in range(len(distance_step_vector)):
+    angle_seg.append((distance_step_vector[d] * r[d]) * (180 / math.pi))
+
+angle_heading = numpy.cumsum(angle_seg)
+angle_heading = list(angle_heading.tolist())
+
+dh = []
+
+temp = angle_heading[-1] % (360 * (abs(angle_heading[-1]) / angle_heading[-1]))
+dh.append(temp)
+temp = angle_heading[-1] - (360 * (abs(angle_heading[-1]) / angle_heading[-1]))
+dh.append(temp)
+
+dh = min(dh)
+
+for ah in range(len(angle_heading)):
+    angle_heading[ah] -= x[ah] / total_length * dh
+
+angle_seg.clear()
+angle_seg.append(angle_heading[0])
+diff_as = numpy.diff(angle_heading)
+diff_as = list(diff_as.tolist())
+angle_seg.extend(diff_as)
+
+temp = angle_heading[0]
+
+for ah in range(len(angle_heading)):
+    angle_heading[ah] -= temp
+
+for i in range(1, len(x)):
+    p = [X[i - 1], Y[i - 1]]
+
+    xyz = helper_lib.rotz(angle_heading[i - 1], distance_step_vector[i - 1], p)
+
+    X[i] = xyz[0]
+    Y[i] = xyz[1]
+
+r_abs = helper_lib.absol(r)
+r_apex_indices = scipy.signal.find_peaks(r_abs)
+r_apex_indices = r_apex_indices[0]
+r_apex_indices = r_apex_indices.tolist()
+
+r_apex = helper_lib.find(r_apex_indices, r)
+
+X = helper_lib.calc_d(x, total_length, X)
+Y = helper_lib.calc_d(x, total_length, Y)
+Z = [0] * len(X)
+
+########################################################################################################
+########################################################################################################
+########################################################################################################
 
 
 f = open("motor.csv", "r")
@@ -229,4 +470,8 @@ for i in range(len(v)):
     GGV[i, :, 0] = numpy.concatenate([ax_acc, ax_dec[1:]])
     GGV[i, :, 1] = numpy.concatenate([ay, numpy.flipud(ay[1:])])
     GGV[i, :, 2] = v[i] * numpy.ones(2 * N - 1)
-print("debug")
+
+######################################################################################################
+######################################################################################################
+######################################################################################################
+
